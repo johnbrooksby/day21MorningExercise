40bd302dd232908173c5cf5949b53f0fca3ec0b0
Anna Brooksby <johnhbrooksby@yahoo.com>
2023-03-13 10:57:18 -0600

    Added Ben's solution

--- oddOneOut.js
+++ oddOneOut.js
@@ -3,6 +3,30 @@ let evenCount = 0
 let doesntBelong = 0
 
 function oddOneOut (str) {
+    // Ben's solution
+    // function outlier(arr){
+    //     let isEven = false;
+        
+    //     let even = 0;
+    //     let odd = 0;
+        
+    //     arr[0] % 2 === 0? even++: odd++;
+    //     arr[1] % 2 === 0? even++: odd++;
+    //     arr[2] % 2 === 0? even++: odd++;
+        
+    //     even > odd? isEven=true : isEven=false;
+      
+    //     let answer = arr.filter(num => {
+    //       if(!isEven) {
+    //         return num % 2 === 0
+    //       }else{
+    //         return num % 2 !== 0
+    //       }
+    //     });
+      
+    //     return answer;
+    //   }
+
     for (let i = 0; i < str.length; i++){
         if (str[i] % 2 === 0) {
             evenCount++
